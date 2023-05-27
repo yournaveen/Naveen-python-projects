1c5b902d05dec985955a80bd3cfcd6aa605c51dc
Naveen dev <118104939+yournaveen@users.noreply.github.com>
2023-05-27 08:13:30 +0530

    Add files via upload

---
+++ STRING.py
@@ -0,0 +1,23 @@
+a = "String is mean to be value of any variable or a group of characters"
+print(a)
+b = "There are some of function that is used to make changes in string and they are"
+print(b)
+c = "Length of the string here space is also a character that has taken"
+print(c,len(c))
+d = "  strip is another one more function that is used remove unwanted space in string "
+print(d,str(d))
+e = " strip itself has two funtions lstrip & rstrip"
+print(e,(str(e)))
+f="lstr removes the left space & rstr function removes the right space"
+print(f)
+g="another funtion is upper which is used to make the letter to capital"
+print(g.upper())
+h="Another Funtion is Lower Which is Used to Make the Letter to Small Letter"
+print(h.lower())
+i="if you want any of your 1st letter is to be capital use this see if letter changes"
+print(i.capitalize())
+j="Another Funtion is count Which is Used to find how many individual character you have in the string"
+print(j)
+print(j.count("i"))
+k="there are so many function keys used to make changes in strings"
+print(k)
